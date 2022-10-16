b6f3f42eb1aa489704304770a0780e7bf123be92
Rafael de Souza <112446497+Rafael-Urei@users.noreply.github.com>
2022-10-16 20:12:29 -0300

    Atualizei os DropDown Menus

--- frontmentord1/scriptjs.js
+++ frontmentord1/scriptjs.js
@@ -1,6 +1,30 @@
+let arrow_up = document.getElementById("arrow_down")
+let arrow_up2 = document.getElementById("arrow_down2")
+let dropdown_menu = document.getElementById("dropmenu_f")
+let dropdown_menuc = document.getElementById("dropmenu_c")
 let menu = document.getElementById("side_menu")
 let menu_links = document.getElementById("side_menu_structure")
 var closebtn = document.getElementById("closebtn")
+
+function dropmenu() {
+    if (dropdown_menu.style.display == "none") {
+        dropdown_menu.style.display = "flex"
+        arrow_up.src="images/icon-arrow-up.svg"
+    } else {
+        dropdown_menu.style.display = "none"
+        arrow_up.src="images/icon-arrow-down.svg"
+    }
+}
+
+function dropmenu2() {
+    if (dropdown_menuc.style.display == "none") {
+        dropdown_menuc.style.display = "flex"
+        arrow_up2.src="images/icon-arrow-up.svg"
+    } else {
+        dropdown_menuc.style.display = "none"
+        arrow_up2.src="images/icon-arrow-down.svg"
+    }
+}
 
 function openMenu() {
     if (menu.style.display == "none"){
