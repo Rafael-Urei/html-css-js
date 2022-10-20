7793ebd7319cea482df14160073b71e3edb7ac57
Rafael de Souza <112446497+Rafael-Urei@users.noreply.github.com>
2022-10-19 22:58:35 -0300

    Finalizei o desafio do frontend mentor

--- frontmentord1/scriptjs.js
+++ frontmentord1/scriptjs.js
@@ -1,18 +1,35 @@
 let arrow_up = document.getElementById("arrow_down")
 let arrow_up2 = document.getElementById("arrow_down2")
+let arrow_up3 = document.getElementById("arrow_down3")
+let arrow_up4 = document.getElementById("arrow_down4")
 let dropdown_menu = document.getElementById("dropmenu_f")
 let dropdown_menuc = document.getElementById("dropmenu_c")
 let menu = document.getElementById("side_menu")
 let menu_links = document.getElementById("side_menu_structure")
 var closebtn = document.getElementById("closebtn")
+let ibackground = document.getElementById("ibackground")
+var sidelinkscontainer = document.getElementById("sidelist_container")
+var sidelinksimg = document.getElementById("sidemenu_menu_img")
+var sidelinksf = document.getElementById("sidelist_links_f")
+var sidelinksc = document.getElementById("sidelist_links_c")
+    
+sidelinksf.style.display = "none"
+sidelinksc.style.display = "none"
+dropdown_menuc.style.display = "none"
+dropdown_menu.style.display = "none"
+menu_links.style.display = "none"
+closebtn.style.display = "none"
+menu.style.display = "none"
 
 function dropmenu() {
     if (dropdown_menu.style.display == "none") {
         dropdown_menu.style.display = "flex"
         arrow_up.src="images/icon-arrow-up.svg"
+        arrow_up.alt="images/icon-arrow-up.svg"
     } else {
         dropdown_menu.style.display = "none"
         arrow_up.src="images/icon-arrow-down.svg"
+        arrow_up.alt="images/icon-arrow-down.svg"
     }
 }
 
@@ -20,9 +37,11 @@ function dropmenu2() {
     if (dropdown_menuc.style.display == "none") {
         dropdown_menuc.style.display = "flex"
         arrow_up2.src="images/icon-arrow-up.svg"
+        arrow_up2.alt="images/icon-arrow-up.svg"
     } else {
         dropdown_menuc.style.display = "none"
         arrow_up2.src="images/icon-arrow-down.svg"
+        arrow_up2.alt="images/icon-arrow-down.svg"
     }
 }
 
@@ -31,6 +50,7 @@ function openMenu() {
         menu.style.display = "flex"
         menu_links.style.display = "flex"
         closebtn.style.display = "flex"
+        ibackground.style.display = "block"
     } else {
         menu.style.display = "none"
         menu_links.style.display = "none"
@@ -41,5 +61,32 @@ function close_menu() {
     if (menu.style.display == "flex") {
         menu.style.display = "none"
         closebtn.style.display = "none"
+        ibackground.style.display = "none"
+    }
+}
+
+function openlist() {
+    if (sidelinksf.style.display == "none") {
+        sidelinksf.style.display = "flex"
+        sidemenu_menu_img.style.display = "flex"
+        arrow_up3.src="images/icon-arrow-up.svg"
+        arrow_up3.alt="images/icon-arrow-up.svg"
+    } else {
+        sidelinksf.style.display = "none"
+        sidemenu_menu_img.style.display = "none"
+        arrow_up3.src="images/icon-arrow-down.svg"
+        arrow_up3.alt="images/icon-arrow-down.svg"
+    }
+}
+
+function openlistc() {
+    if (sidelinksc.style.display == "none") {
+        sidelinksc.style.display = "flex"
+        arrow_up4.src="images/icon-arrow-up.svg"
+        arrow_up4.alt="images/icon-arrow-up.svg"
+    } else {
+        sidelinksc.style.display = "none"
+        arrow_up4.src="images/icon-arrow-down.svg"
+        arrow_up4.alt="images/icon-arrow-down.svg"
     }
 }
